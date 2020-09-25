667636c2679f89a5929cf422a7ae0f49f320895d
holdMyGlasses <dusan.rula@gmail.com>
2020-09-25 12:59:04 -0400

    pwd working

--- bash.js
+++ bash.js
@@ -6,5 +6,9 @@ process.stdin.on('data', (data) => {
     process.stdout.write('You typed: ' + cmd)
     process.stdout.write('\nprompt > ')
 
-})
+    if (cmd === 'pwd'){
+        process.stdout.write('\n your current directory is: ' + process.cwd())
+        process.stdout.write('\nprompt > ')
+    }
 
+})
